6e5e1fd961c3b20576ea3fcb17dfc6cd8bd3f2f5
Lawkodana <158286191+Lawkodana@users.noreply.github.com>
2024-01-31 12:06:30 +0000

    first init

--- main.py
+++ main.py
@@ -12,21 +12,19 @@ luxurious_lamp_description = "Lyxig Lampa i glas och järn. Stående 91 cm hög,
 
 luxurious_lamp_price = 52.15
 
-sales_tax = *00.088
+sales_tax = 0.088
 
 customer_one_total = 0
 
 customer_one_itemization = ""
 
-customer_one_total = 
-
 customer_one_itemization = lovely_loveseat_description + luxurious_lamp_description
 
-customer_one_total = luxurious_lamp_price 
+customer_one_total = luxurious_lamp_price + lovely_loveseat_price
 
 customer_one_tax = customer_one_total * sales_tax
 
 
 
-print("varor för kund ett: " + customer_one_itemization)
-print("totalt för kund ett: " + customer_one_total) 
+print("varor för kund ett:", customer_one_itemization)
+print("totalt för kund ett:", customer_one_total)
